f4e26d3d2b5ccc98af273e7b77ea9d872fad66b1
Tushar <tushar.gangurde92@gmail.com>
2021-07-22 20:46:41 +0530

    custom response message added for errors and exceptions

--- src/main/java/com/tushar/lms/apigateway/security/CustomAuthorizationFilter.java
+++ src/main/java/com/tushar/lms/apigateway/security/CustomAuthorizationFilter.java
@@ -1,16 +1,22 @@
 package com.tushar.lms.apigateway.security;
 
+import java.nio.charset.StandardCharsets;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.server.ServerWebExchange;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureException;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Configuration
@@ -36,18 +42,58 @@ public class CustomAuthorizationFilter implements GlobalFilter {
 
 			if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer")) {
 				logger.info("Authorization header is empty or does not start with Bearer");
-				response.setStatusCode(HttpStatus.UNAUTHORIZED);
-				return response.setComplete();
+				return Mono.defer(() -> {
+					response.setStatusCode(HttpStatus.UNAUTHORIZED);// Set status
+
+					byte[] bytes = "No token present".getBytes(StandardCharsets.UTF_8);
+					DataBuffer buffer = response.bufferFactory().wrap(bytes);
+					logger.info("No token present");
+					return response.writeWith(Flux.just(buffer));// Set body
+				});
+
 			}
 
 			String token = authorizationHeader.replace("Bearer ", "");
-
-			String userId = Jwts.parser().setSigningKey("secret_key").parseClaimsJws(token).getBody().getSubject();
+			String userId = null;
+			
+			try {
+				userId = Jwts.parser().setSigningKey("secret_key").parseClaimsJws(token).getBody().getSubject();
+			} catch (ExpiredJwtException exception) {
+				return Mono.defer(() -> {
+					response.setStatusCode(HttpStatus.UNAUTHORIZED);// Set status
+					byte[] bytes = "JWT Token Expired".getBytes(StandardCharsets.UTF_8);
+					DataBuffer buffer = response.bufferFactory().wrap(bytes);
+					logger.info("JWT Token Expired");
+					return response.writeWith(Flux.just(buffer));// Set body
+				});
+			} catch (SignatureException exception) {
+				return Mono.defer(() -> {
+					response.setStatusCode(HttpStatus.UNAUTHORIZED);// Set status
+					byte[] bytes = "JWT signature does not match".getBytes(StandardCharsets.UTF_8);
+					DataBuffer buffer = response.bufferFactory().wrap(bytes);
+					logger.info("JWT signature does not match");
+					return response.writeWith(Flux.just(buffer));// Set body
+				});
+			} catch (Exception exception) {
+				return Mono.defer(() -> {
+					response.setStatusCode(HttpStatus.UNAUTHORIZED);// Set status
+					byte[] bytes = ("JWT related exception. " + exception.getMessage())
+							.getBytes(StandardCharsets.UTF_8);
+					DataBuffer buffer = response.bufferFactory().wrap(bytes);
+					logger.info(exception.getMessage());
+					return response.writeWith(Flux.just(buffer));// Set body
+				});
+			}
 
 			if (userId == null) {
-				logger.info("User Id not present in the JWT");
-				response.setStatusCode(HttpStatus.UNAUTHORIZED);
-				return response.setComplete();
+				return Mono.defer(() -> {
+					response.setStatusCode(HttpStatus.UNAUTHORIZED);// Set status
+
+					byte[] bytes = "User Id is not present".getBytes(StandardCharsets.UTF_8);
+					DataBuffer buffer = response.bufferFactory().wrap(bytes);
+					logger.info("User Id is not present");
+					return response.writeWith(Flux.just(buffer));// Set body
+				});
 			}
 
 			return chain.filter(exchange);
